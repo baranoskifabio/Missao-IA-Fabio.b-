c170348b8b277eb19a59a9a56402115773b744c6
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-10-07 12:17:23 -0300

    ...

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ let atual = 0
 let perguntaAtual;
 
 function mostraPergunta(){
-    perguntaAtual = perguntas [atual]
+    perguntaAtual = perguntas [atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
