4fd8ffc49f18687bbc1d43425f62a6cb714a8f52
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-10-21 11:54:04 -0300

    ..

--- main.js
+++ main.js
@@ -87,17 +87,18 @@ function mostraPergunta(){
     if(atual >= perguntas.length){
         mostraResultado();
         return;
-        
+
     }
 
     perguntaAtual = perguntas [atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement('button');
+        const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas);
